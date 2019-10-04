09c02d3eff112737b11494e199a17bade17b554a
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-04 10:18:24 +0100

    Unit test App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+
+let tools;
+
+beforeEach(() => {
+  rtl.cleanup();
+  tools = rtl.render(<App />);
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Contains the right header', () => {
+  const elementWithNameText = tools.queryByText(/Women's World Cup/i);
+  expect(elementWithNameText).toBeInTheDocument();
 });
