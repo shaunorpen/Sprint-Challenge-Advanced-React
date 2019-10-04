e7112ed1f7bdfb460f671226a9e532a5b53b4579
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-04 12:36:38 +0100

    Refactor App to class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './App.css';
-
-import { playerData } from './data/data';
-
 import CountryCard from './components/CountryCard';
 
-const countryList = (playerData) => {
-  const countries = [];
-  playerData.map(player => {
-    if (!countries.includes(player.country)) {
-      countries.push(player.country)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerData: [],
     };
-  });
-  return countries.sort();
-}
+  }
 
-const App = () => {
-  return (
-    <>
-      <h1>Women's World Cup</h1>
-      <div className="App">
-        { countryList(playerData).map(country => 
-            <CountryCard 
-              country={country} 
-              key={country}
-              players={playerData.filter(player => player.country === country)} 
-            />
-          ) 
-        }
-      </div>
-    </>
-  );
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then(res => {
+        this.setState({
+          playerData: res.data,
+        });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
+
+  countryList = (playerData) => {
+    const countries = [];
+    playerData.map(player => {
+      if (!countries.includes(player.country)) {
+        countries.push(player.country)
+      };
+    });
+    return countries.sort();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Women's World Cup</h1>
+        <div className="App">
+          { this.countryList(this.state.playerData).map(country => 
+              <CountryCard 
+                country={country} 
+                key={country}
+                players={this.state.playerData.filter(player => player.country === country)} 
+              />
+            ) 
+          }
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
