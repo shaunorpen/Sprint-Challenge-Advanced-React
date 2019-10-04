db9ef4a9638d663d0be6bea4e9269409540c3e54
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-04 09:38:34 +0100

    Data rendering to screen

--- client/src/App.js
+++ client/src/App.js
@@ -13,17 +13,24 @@ class App extends React.Component {
   componentDidMount() {
     axios.get('http://localhost:5000/api/players')
       .then(res => {
-        this.state.worldCupData = res.data;
+        this.setState({
+          worldCupData: res.data
+        });
       })
       .catch(err => {
         alert(err.message);
       })
-    }
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Women's World Cup</h1>
+        {
+          this.state.worldCupData.map(player => {
+            return <div>{player.name}</div>
+          })
+        }
       </div>
     )
   }
