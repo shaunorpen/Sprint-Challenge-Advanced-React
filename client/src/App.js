36405ab34cb9658b19ba6f37320197a4d6db9d34
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-04 08:58:34 +0100

    Data returning from the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      worldCupData: [],
+    };
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then(res => {
+        this.state.worldCupData = res.data;
+      })
+      .catch(err => {
+        alert(err.message);
+      })
+    }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Women's World Cup</h1>
       </div>
     )
   }
